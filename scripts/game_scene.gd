25608526b307f401a69f105904c11a372755f57c
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2025-07-18 18:19:09 -0400

    Prep for collision

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CanvasLayer
 
 @onready var playerCharacterScene = preload("res://scenes/player_characters.tscn")
 
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -2,3 +2,7 @@ extends Node
 
 @export var tileSize: int = 16
 @export var numCharacters: int = 3
+@export var characterStepSize: int = 1
+#@export var wallCollisionLayer: int = 1
+#@export var hazardCollisionLayer: int = 2
+#@export var playerCollisionLayer: int = 3
